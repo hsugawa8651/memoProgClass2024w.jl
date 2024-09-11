ae65467719d471463583cb6273d31d90d868f026
Documenter.jl <documenter@juliadocs.github.io>
2024-09-11 09:09:30 +0000

    build based on fc4500e

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = memoProgClass2024w","category":"page"},{"location":"#memoProgClass2024w","page":"Home","title":"memoProgClass2024w","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for memoProgClass2024w.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [memoProgClass2024w]","category":"page"}]
+}
